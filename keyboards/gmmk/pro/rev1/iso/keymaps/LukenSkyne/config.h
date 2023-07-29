6948cc881c9902d302fc5b006db1ba727c4ec107
Luken <github@luken.cc>
2023-07-29 18:29:45 +0200

    Add custom keymap

---
+++ keyboards/gmmk/pro/rev1/iso/keymaps/LukenSkyne/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+// default polling rate is 1ms
+//#define USB_POLLING_INTERVAL_MS 1
+
+// default debounce time is 5ms
+//#define DEBOUNCE 5
+
+// use this to disable lights when computer goes to sleep
+#define RGB_DISABLE_WHEN_USB_SUSPENDED
+
+// allow the keyboard to hold down 248 keys at once (default NKRO is 6)
+#define FORCE_NKRO
